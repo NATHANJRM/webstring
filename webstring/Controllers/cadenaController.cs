e112ad4546c4455b76b2543170301411ad9547fe
JJRM <JJRM@JONATHAN>
2023-06-25 18:01:38 -0600

    Agregar archivos de proyecto.

---
+++ webstring/Controllers/cadenaController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webstring.Models;
+
+namespace webstring.Controllers
+{
+    public class cadenaController : Controller
+    {
+        // GET: cadena
+        public ActionResult practica()
+        {
+            return View();
+        }
+
+        public ActionResult contar(string valor1)
+        {
+            logica obj = new logica();
+            string resultado1 = obj.contar(valor1);
+            TempData["contar"] = resultado1;
+            return View("practica");
+        }
+
+        public ActionResult mayuscula(string valor2)
+        {
+            logica obj = new logica();
+            string resultado2 = obj.mayuscula(valor2);
+            TempData["mayuscula"] = resultado2;
+            return View("practica");
+        }
+
+        public ActionResult minuscula(string valor3)
+        {
+            logica obj = new logica();
+            string resultado3 = obj.minuscula(valor3);
+            TempData["minuscula"] = resultado3;
+            return View("practica");
+        }
+
+        public ActionResult cajas(string valor4, string valor5, string valor6)
+        {
+            logica obj = new logica();
+            string resultado4 = $"{valor4} {valor5} {valor6}";
+            TempData["cajas"] = resultado4;
+            return View("practica");
+        }
+
+        public ActionResult validar(string valor7, string valor8)
+        {
+            logica obj = new logica();
+            string resultado5 = obj.validar(valor7, valor8);
+            TempData["validar"] = resultado5;
+            return View("practica");
+        }
+
+        public ActionResult cadena(string valor9)
+        {
+            logica obj = new logica();
+            string resultado6 = obj.cadena(valor9);
+            TempData["cadena"] = resultado6;
+            return View("practica");
+        }
+
+        public ActionResult coma(string valor10)
+        {
+            logica obj = new logica();
+            string resultado7 = obj.coma(valor10);
+            TempData["coma"] = resultado7;
+            return View("practica");
+        }
+
+        public ActionResult espacio(string valor11)
+        {
+            logica obj = new logica();
+            string resultado8 = obj.espacio(valor11);
+            TempData["espacio"] = resultado8;
+            return View("practica");
+        }
+
+        public ActionResult ti(string valor12)
+        {
+            logica obj = new logica();
+            string resultado9 = obj.ti(valor12);
+            TempData["ti"] = resultado9;
+            return View("practica");
+        }
+
+        public ActionResult ejercicio10(string valor13)
+        {
+            logica obj = new logica();
+            string resultado10 = obj.ejercicio10(valor13);
+            TempData["ejercicio10"] = resultado10;
+            return View("practica");
+        }   
+    }
+}
---
+++ webstring/Models/logica.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webstring.Models
+{
+    public class logica
+    {
+        /*ejercicio 1*/
+        public string contar(string valor1)
+        {
+            int dato = valor1.Count();
+            string resultado1 = $"{valor1} tiene {dato} caracteres";
+            return resultado1;
+        }
+
+        /*ejercicio 2*/
+        public string mayuscula(string valor2)
+        {
+            string resultado2 = valor2.ToUpper();
+            return resultado2;
+        }
+
+        /*ejercicio 3*/
+        public string minuscula(string valor3)
+        {
+            string resultado3 = valor3.ToLower();
+            return resultado3;
+        }  
+        
+        /*ejercicio 4*/
+        public string cajas(string valor4, string valor5, string valor6)
+        {
+            string resultado4 = $"{valor4} {valor5} {valor6}";
+            return resultado4;            
+        }
+
+        /*ejercicio 5*/
+        public string validar(string valor7, string valor8)
+        {
+            if (valor7.StartsWith(valor8))
+            {
+
+            }
+            else if(valor8.StartsWith(valor7))
+            {
+                    
+            }
+            string resultado5 = valor7;
+            return resultado5;
+        }
+
+        /*ejercicio 6*/
+        public string cadena(string valor9)
+        {
+            string resultado6 = valor9.Replace(',', ' ').Replace('/', ' ');            
+            return resultado6;
+        }
+
+        /*ejercicio 7*/
+        public string coma(string valor10)
+        {
+            string resultado7 = valor10.TrimEnd(',', ' ');
+            return resultado7;
+        }
+
+        /*ejercicio 8*/
+        public string espacio(string valor11)
+        {
+            string resultado8 = valor11.Trim();
+            return resultado8;
+        }
+
+        /*ejercicio 9*/
+        public string ti(string valor12)
+        {
+            string resultado9 = valor12;
+            if (valor12.StartsWith("ti"))
+            {
+                resultado9 = $"El enunciado {valor12} SI empieza con la palabra TI";
+            }
+            else
+            {
+                resultado9 = $"El enunciado {valor12} NO empieza con la palabra TI";
+            }           
+            return resultado9;
+        }
+
+        /*ejercicio10*/
+        public string ejercicio10(string valor13)
+        {
+            string resultado10 = valor13.PadLeft(5, '0');
+            return resultado10;
+        }      
+    }
+}
